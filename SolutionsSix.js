5a906ecfa411e79a4c2e9f022d4a5860ffc8c5a8
Eganathan R <ammasamrith@gmail.com>
2022-03-18 18:15:20 +0530

    Create SolutionsSix.js

---
+++ SolutionsSix.js
@@ -0,0 +1,16 @@
+//sort Array lowest to largest
+const nums = [1,4,3,2,5,6];
+
+sortAssending(nums);
+sortDessending(nums);
+
+function sortAssending(arr)
+{
+    console.log(arr.sort());
+}
+
+//sort array in reverse order
+function sortDessending(arr)
+{
+    console.log(arr.reverse());
+}
