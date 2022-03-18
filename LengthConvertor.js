4053a914c27a010b8ca12086a09269ceba6d7306
Eganathan R <ammasamrith@gmail.com>
2022-03-18 17:54:30 +0530

    Create LengthConvertor.js

---
+++ LengthConvertor.js
@@ -0,0 +1,38 @@
+//meteres to centimeters
+metersToCm(34);
+function metersToCm(meters)
+{
+    let cm = meters*100;
+    console.log("Meters to centimeter");
+    console.log(meters+" mtr ->  "+cm+" cm");
+    console.log(" ");
+}
+//Centimeters to Meters
+cmToMeters(1500);
+function cmToMeters(cm)
+{
+    let meters = cm/100;
+    console.log("Centimeter to Meters");
+    console.log(meters+" cm -> "+meters+" mtr");
+    console.log(" ");
+}
+//meteres to Km
+mtrToKm(1500);
+function mtrToKm(meters)
+{
+    let km = mters*100;
+    console.log("Meters to Kilometers");
+    console.log(meters+"m -> "+km+" Km");
+    console.log(" ");
+}
+
+// =========================Temperature
+//Celsius to Fahrenheit
+celciousToF(56);
+function celciousToF(celsius)
+{
+    let f = (celsius* 9/5)+32;
+    console.log("celsius to Fahrenheit");
+    console.log(celsius+"c -> "+f+"F");
+    console.log(" ");
+}
